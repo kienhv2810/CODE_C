ee4cbdbcd7545e4fa8947edb9a278f2ddadd0ae8
Ha Van Kien <kienhv102@gmail.com>
2026-05-14 17:15:47 +0700

    Update giaiThua.cpp

--- Functions/giaiThua.cpp
+++ Functions/giaiThua.cpp
@@ -1,13 +1,4 @@
-int ucln(int a,int b){
-	if(a == 0) return b;
-	if(b == 0) return a;
-	while(a != b){
-		if(a > b) a -= b;
-		else b -= a;
-	}
-	return a;
-}
-int bcnn(int a,int b){
-	if(a == 0 || b == 0) return 0;
-	return (a/ucln(a,b))*b;
+int giaiThua(int n){
+	if(n == 1) return 1;
+	return n*giaiThua(n-1);
 }
